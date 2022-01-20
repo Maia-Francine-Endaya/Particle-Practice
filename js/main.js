61efcc3928c179678f294232bab6c15534c1e697
Maia Francine Endaya <maia.endaya@gmail.com>
2022-01-20 21:42:28 +0800

    added_particle_effects

--- js/main.js
+++ js/main.js
@@ -1,24 +1,64 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-
-scene.background = new THREE.Color(0x6d777d);
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const light = new THREE.HemisphereLight(0xffffbb, 0x3b3d80, 1);
-scene.add(light);
+let nameMesh = new THREE.Mesh();
+let stars, starGeo;
 
-const woodenTexture = new THREE.TextureLoader().load('assets/textures/wood_texture.jpg');
+lighting();
+name();
+particles();
 
-const boxGeometry = new THREE.BoxGeometry(15, 5, 10);
-const boxMaterial = new THREE.MeshPhongMaterial({ map: woodenTexture });
-const box = new THREE.Mesh(boxGeometry, boxMaterial);
-scene.add(box);
+function particles() {
+  const points = [];
 
-camera.position.z = 35;
+  for (let i = 0; i < 6000; i++) {
+    let star = new THREE.Vector3(
+      Math.random() * 600 - 300,
+      Math.random() * 600 - 300,
+      Math.random() * 600 - 300
+    );
+    points.push(star);
+  }
+
+  starGeo = new THREE.BufferGeometry().setFromPoints(points);
+
+  let sprite = new THREE.TextureLoader().load("assets/star.png");
+  let starMaterial = new THREE.PointsMaterial({
+    color: 0xffb6c1,
+    size: 0.7,
+    map: sprite,
+  });
+
+  stars = new THREE.Points(starGeo, starMaterial);
+  scene.add(stars);
+}
+
+function animateParticles() {
+  starGeo.verticesNeedUpdate = true;
+  stars.position.y -= 0.9;
+}
+
+
+function name() {
+  const woodenTexture = new THREE.TextureLoader().load('assets/textures/wood_texture.jpg');
+  const boxGeometry = new THREE.BoxGeometry(15, 5, 10);
+  const boxMaterial = new THREE.MeshPhongMaterial({ map: woodenTexture });
+  nameMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+  box.position.z = -5;
+  camera.position.z = 35;
+
+  scene.add(nameMesh);
+}
+
+function lighting() {
+  const light = new THREE.HemisphereLight(0xffffbb, 0x3b3d80, 1);
+  scene.add(light);
+}
 
 function animate() {
   requestAnimationFrame(animate);
