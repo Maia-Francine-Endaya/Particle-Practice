51fe9adc7863b6e81d0ea86b7bfa45301c903a6f
Maia Francine Endaya <maia.endaya@gmail.com>
2022-01-24 14:37:46 +0800

    removed_box

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 10, 1000);
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let nameMesh = new THREE.Mesh();
+let cubeMesh = new THREE.Mesh();
 let stars, starGeo;
 
 lighting();
-name();
 particles();
 
 function particles() {
@@ -42,30 +41,23 @@ function animateParticles() {
   stars.position.y -= 0.9;
 }
 
-
-function name() {
-  const woodenTexture = new THREE.TextureLoader().load('assets/textures/wood_texture.jpg');
-  const boxGeometry = new THREE.BoxGeometry(15, 5, 10);
-  const boxMaterial = new THREE.MeshPhongMaterial({ map: woodenTexture });
-  nameMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-
-  box.position.z = -5;
-  camera.position.z = 35;
-
-  scene.add(nameMesh);
-}
-
 function lighting() {
-  const light = new THREE.HemisphereLight(0xffffbb, 0x3b3d80, 1);
+  const light = new THREE.HemisphereLight(0xffca99, 0x9a53e6, 1);
   scene.add(light);
+
+  const pointLight = new THREE.PointLight(0xff0000, 1, 100);
+  pointLight.position.set(50, 50, 50);
+  scene.add(pointLight);
 }
 
 function animate() {
   requestAnimationFrame(animate);
 
-  box.rotation.x += 0.01;
-  box.rotation.y += 0.01;
+  animateParticles();
 
+  cubeMesh.rotation.x += 0.008;
+  cubeMesh.rotation.y += 0.008;
   renderer.render(scene, camera);
 }
+
 animate();
